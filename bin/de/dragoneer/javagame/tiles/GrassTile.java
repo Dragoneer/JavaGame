e612031ea9cfe611e6e2991c28b511e6e66a4b80
Dragoneer <Dragoneer@users.noreply.github.com>
2015-04-01 18:17:18 +0000

    Create GrassTile.java

---
+++ bin/de/dragoneer/javagame/tiles/GrassTile.java
@@ -0,0 +1,11 @@
+package de.dragoneer.javagame.tiles;
+
+import de.dragoneer.javagame.gfx.Assets;
+
+public class GrassTile extends Tile {
+
+	public GrassTile(int id) {
+		super(Assets.grass, id);
+	}
+
+}
