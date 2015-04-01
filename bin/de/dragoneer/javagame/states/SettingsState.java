0a1707f3cb8ab5fa4a4761bd037802300c6a049f
Dragoneer <Dragoneer@users.noreply.github.com>
2015-04-01 18:15:23 +0000

    Create SettingsState.java

---
+++ bin/de/dragoneer/javagame/states/SettingsState.java
@@ -0,0 +1,23 @@
+package de.dragoneer.javagame.states;
+
+import java.awt.Graphics;
+
+import de.dragoneer.javagame.Game;
+
+public class SettingsState extends State{
+
+	public SettingsState(Game game){
+		super(game);
+	}
+	
+	@Override
+	public void tick() {
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		
+	}
+	
+}
