aeda70fe074278156748b815ebb3daf8dad635b4
Dragoneer <Dragoneer@users.noreply.github.com>
2015-04-01 18:16:41 +0000

    Create DirtTile.java

---
+++ bin/de/dragoneer/javagame/tiles/DirtTile.java
@@ -0,0 +1,11 @@
+package de.dragoneer.javagame.tiles;
+
+import de.dragoneer.javagame.gfx.Assets;
+
+public class DirtTile extends Tile {
+
+	public DirtTile(int id) {
+		super(Assets.dirt, id);
+	}
+
+}
