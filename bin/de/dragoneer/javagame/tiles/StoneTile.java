f7307b2fcfa0e8a72c2cdae6782d471f0c90a666
Dragoneer <Dragoneer@users.noreply.github.com>
2015-04-01 18:17:53 +0000

    Create StoneTile.java

---
+++ bin/de/dragoneer/javagame/tiles/StoneTile.java
@@ -0,0 +1,11 @@
+package de.dragoneer.javagame.tiles;
+
+import de.dragoneer.javagame.gfx.Assets;
+
+public class StoneTile extends Tile {
+
+	public StoneTile(int id) {
+		super(Assets.stone, id);
+	}
+
+}
