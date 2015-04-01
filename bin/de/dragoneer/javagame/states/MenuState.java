d5328dbbbb98d7fa7a2f70db899f2b2beb0233fc
Dragoneer <Dragoneer@users.noreply.github.com>
2015-04-01 18:15:05 +0000

    Create MenuState.java

---
+++ bin/de/dragoneer/javagame/states/MenuState.java
@@ -0,0 +1,25 @@
+package de.dragoneer.javagame.states;
+
+import java.awt.Graphics;
+
+import de.dragoneer.javagame.Game;
+
+public class MenuState extends State {
+
+	public MenuState(Game game){
+		super(game);
+	}
+	
+	@Override
+	public void tick() {
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		
+	}
+
+	
+	
+}
