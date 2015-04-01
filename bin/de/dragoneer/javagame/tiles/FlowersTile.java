14cef705b85b10a81864104bea522110a9b21f72
Dragoneer <Dragoneer@users.noreply.github.com>
2015-04-01 18:17:00 +0000

    Create FlowersTile.java

---
+++ bin/de/dragoneer/javagame/tiles/FlowersTile.java
@@ -0,0 +1,11 @@
+package de.dragoneer.javagame.tiles;
+
+import de.dragoneer.javagame.gfx.Assets;
+
+public class FlowersTile extends Tile {
+
+	public FlowersTile(int id) {
+		super(Assets.flowers, id);
+	}
+
+}
